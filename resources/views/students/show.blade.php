6995f5be10af5742f07c248f21a3a28638a8f903
Sudar22 <cndemso25@outlook.com>
2024-10-28 10:44:08 +0530

    students page done

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -3,20 +3,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1>Student Details</h1>
+@include('layouts.bigHeading', ['submenuName' => $student->submenu->name ])
 
-    <div class="card">
+<div class="container">
+
+
+    <div class="card mt-10">
         <div class="card-body">
-            <h3>Menu: {{ $student->menu->name  }}</h3>
+            <table class="min-w-full bg-white border border-gray-300">
+                <thead >
+                    <tr class="bg-gray-200 h-10">
+
+                        <th>Name</th>
+                        <th>Photo</th>
+                        <th>Period</th>
+                        <th>Field</th>
+                        <th>Guide</th>
+
+                        
+                    </tr>
+                </thead>
+            <!-- <h3>Menu: {{ $student->menu->name  }}</h3>
             <h4>Submenu: {{ $student->submenu->name  }}</h4>
 
-            <h5>Students:</h5>
-            <ul>
+            <h5>Students:</h5> -->
+            <tbody>
                 @foreach ($student->students as $student)
-                <li>{{ $student['name'] }}</li>
+                <tr class="h-24">
+                <td class="text-center">{{ $student['name'] }}</td>
+                <td class="justify-center items-center flex flex-col h-24">
+                   <img src="{{ asset('storage/' . $student['photo'] )}}" alt="" class="w-16 rounded-full"> </td>
+                <td class="text-center">{{ $student['from_year'] }}-{{ $student['to_year'] }} </td>
+                <td class="text-center">{{ $student['field'] }}</td>
+                <td class="text-center">{{ $student['guide'] }}</td>
+                </tr>
                 @endforeach
-            </ul>
+            </tbody>
+            </table>
         </div>
     </div>
 
